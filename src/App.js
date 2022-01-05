6342f1e269032f64fe12679278e5f07bd69b352b
Murilo <murilomininel1@hotmail.com>
2022-01-05 17:10:11 -0300

    montando requisicao api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {PegaLista} from "./service/lista"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [alert, serAlert]= useState(false);
+  const [itemInput, setItemInput] = useState('');
+  const [list, FazLista] = useState([]);
+  let montado =true;
+  
+  useEffect(() =>{
+    PegaLista()
+    .then(aluno =>{
+      if(montado){
+        FazLista(aluno)
+      }
+    })
+    return () => montado = false;
+  },[])
+
+  return(
+    <div className= "wrapper">
+    <h1>Lista de alunos</h1>
+    <ul>
+      {list.map(aluno => <li key={aluno.aluno}>{aluno.aluno}</li>)}
+    </ul>
+    <form>
+       <label>
+         <p>Novo Item</p>
+         <input type="text" />
+       </label>
+       <button type="submit">Submit</button>
+     </form>
     </div>
-  );
+  )
 }
-
-//comentario da API teste
-
 export default App;
---
+++ src/service/Alunos.jsx
@@ -0,0 +1,44 @@
+import axios from "axios"
+
+const url= "https://dashboard.heroku.com/apps/api-academia-alunos"
+
+const Alunos= {
+    async listAlunos() {
+        const endpoint = `${url}`
+        return axios.get(endpoint)
+      },
+    
+
+    async listAluno(alunoEmail) {
+        const endpoin = `${url}/${alunoEmail}`
+        return axios.get(endpoin)
+      },
+
+      async addAluno (data) {
+        return axios({
+          method: 'post',
+          baseURL:url,
+          data: data
+        })
+      },
+
+      async editAluno (alunoEmail, data) {
+        const endpoin = `/${alunoEmail}`
+        return axios({
+          method: 'put',
+          url: 'https://dashboard.heroku.com/apps/api-academia-alunos'+ endpoin,
+          data: data
+        })
+      },
+
+      async deleteAluno (alunoEmail) {
+        const endpoin = `/${alunoEmail}`
+        return axios({
+          method: 'delete',
+          url:endpoin,
+          baseURL: url,
+        })
+      },
+    }
+export default Alunos
+//nome, cpf, endereco, numero, bairro, cidade, estado, telefone, email, idade, plano
