ece8cb2bf1276ba205f565def1e69e03b8cf177f
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-05 18:52:06 -0300

    Barra de Navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import './App.css';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-     <h1>Hello!</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to="/">GymTech</NavLogo>
+                <MobileIcon>
+                    <FaBars />    
+                </MobileIcon>
+                <NavMenu>
+                    <NavItem>
+                        <NavLinks to="about">Sobre Nós</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks to='unidades'>Unidades</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks to='planos'>Planos</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks to='atividades'>Atividades</NavLinks>
+                    </NavItem>
+
+                    <NavItem>
+                        <NavLinks to='contato'>Contato</NavLinks>
+                    </NavItem>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/login">Log In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar;
