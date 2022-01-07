e70503d8bd0725f419e129af6b465302c980e212
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-07 02:20:51 -0300

    Início Home

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -7,6 +7,15 @@ const HeroSection = () => {
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>O futuro para construir sua melhor versão</HeroH1>
+                <HeroP>
+                    Conectando seu corpo com tecnologia e ciência para resultados em tempo recorde.
+                    </HeroP>
+                    <HeroBtnWrapper>
+                        <Button to="signup"
+                    </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
