3d90a125a46c3f54840c68d089bb865686634872
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-09 10:36:13 -0300

    Adições Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { FooterContainer, 
     FooterWrap, 
     FooterLinksContainer, 
@@ -14,44 +15,84 @@ const Footer = () => {
                    <FooterLinksWrapper>
                        <FooterLinksItems>
                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='login'>How it works</FooterLink>
-                               <FooterLink to='login'>Testmonials</FooterLink>
-                               <FooterLink to='login'>Carrers</FooterLink>
-                               <FooterLink to='login'>Investors</FooterLink>
-                               <FooterLink to='login'>Terms of Service</FooterLink>                           
+                               <FooterLink to='/'>How it works</FooterLink>
+                               <FooterLink to='/'>Testmonials</FooterLink>
+                               <FooterLink to='/'>Carrers</FooterLink>
+                               <FooterLink to='/'>Investors</FooterLink>
+                               <FooterLink to='/'>Terms of Service</FooterLink>                           
                        </FooterLinksItems>
 
                        <FooterLinksItems>
                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='login'>How it works</FooterLink>
-                               <FooterLink to='login'>Testmonials</FooterLink>
-                               <FooterLink to='login'>Carrers</FooterLink>
-                               <FooterLink to='login'>Investors</FooterLink>
-                               <FooterLink to='login'>Terms of Service</FooterLink>                           
+                               <FooterLink to='/'>How it works</FooterLink>
+                               <FooterLink to='/'>Testmonials</FooterLink>
+                               <FooterLink to='/'>Carrers</FooterLink>
+                               <FooterLink to='/'>Investors</FooterLink>
+                               <FooterLink to='/'>Terms of Service</FooterLink>                           
                        </FooterLinksItems>
                     </FooterLinksWrapper>  
 
                     <FooterLinksWrapper>
                        <FooterLinksItems>
                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='login'>How it works</FooterLink>
-                               <FooterLink to='login'>Testmonials</FooterLink>
-                               <FooterLink to='login'>Carrers</FooterLink>
-                               <FooterLink to='login'>Investors</FooterLink>
-                               <FooterLink to='login'>Terms of Service</FooterLink>                           
+                               <FooterLink to='/'>How it works</FooterLink>
+                               <FooterLink to='/'>Testmonials</FooterLink>
+                               <FooterLink to='/'>Carrers</FooterLink>
+                               <FooterLink to='/'>Investors</FooterLink>
+                               <FooterLink to='/'>Terms of Service</FooterLink>                           
                        </FooterLinksItems>
 
                        <FooterLinksItems>
                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='login'>How it works</FooterLink>
-                               <FooterLink to='login'>Testmonials</FooterLink>
-                               <FooterLink to='login'>Carrers</FooterLink>
-                               <FooterLink to='login'>Investors</FooterLink>
-                               <FooterLink to='login'>Terms of Service</FooterLink>                           
+                               <FooterLink to='/'>How it works</FooterLink>
+                               <FooterLink to='/'>Testmonials</FooterLink>
+                               <FooterLink to='/'>Carrers</FooterLink>
+                               <FooterLink to='/'>Investors</FooterLink>
+                               <FooterLink to='/'>Terms of Service</FooterLink>                           
                        </FooterLinksItems>
 
                     </FooterLinksWrapper>                     
                 </FooterLinksContainer>
+
+                <SocialMedia>
+                    <SocialMediaWrap>
+                    <SocialLogo to='/'>
+                    dolla
+                    </SocialLogo>
+                    <WebsiteRights> GymTech  © {new Date().getFullYear()} 
+                    All rigths reserved</WebsiteRights>
+
+                    <SocialIcons>
+                        <SocialIconLink href="//www.facebook.com/resilia.br/" target="_blank"
+                        aria-label="Facebook">
+                        <FaFacebook />
+                        </SocialIconLink>
+
+                        <SocialIconLink href="//https://www.instagram.com/resilia.br/" target="_blank"
+                        aria-label="Instagram">
+                        <FaInstagram />
+                        </SocialIconLink>
+
+                        <SocialIconLink href="//www.youtube.com/channel/UCBIRxu3dixMNMRVjz-YQAcA" target="_blank"
+                        aria-label="Youtube">
+                        <FaYoutube />
+                        </SocialIconLink>
+
+                        <SocialIconLink href="//twitter.com/resilia_br" target="_blank"
+                        aria-label="Twitter">
+                        <FaTwitter />
+                        </SocialIconLink>
+
+                        <SocialIconLink href="//www.linkedin.com/company/resilia-educa%C3%A7%C3%A3o/" target="_blank"
+                        aria-label="Linkedin">
+                        <FaLinkedin />
+                        </SocialIconLink>
+                        
+                    </SocialIcons>
+
+                    </SocialMediaWrap>
+                    </SocialMedia>
+
             </FooterWrap>
         </FooterContainer>
     );
