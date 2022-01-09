5b26507aa026176cdc841c030a21a690cfab9ff0
Larissa Silva <88165271+LariCostaSilva@users.noreply.github.com>
2022-01-09 13:22:49 -0300

    Scrool alterado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,91 +1,112 @@
 import React from 'react'
-import { FaFacebook, FaInstagram, FaYoutube, FaLinkedin, FaTwitter } from 'react-icons/fa';
-import { FooterContainer, FooterWrap, FooterLinksContainer, FooterLinksWrapper, FooterLinkItems, FooterLinkTitle, FooterLink, SocialMedia, SocialMediaWrap, SocialLogo, WebsiteRights, SocialIcons, SocialIconLink} from './FooterElements';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaYoutube, FaLinkedin,
+    FaTwitter
+}
+    from 'react-icons/fa';
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
+}
+    from './FooterElements';
 const Footer = () => {
     return (
         <FooterContainer>
             <FooterWrap>
                 <FooterLinksContainer>
-                   <FooterLinksWrapper>
-                       <FooterLinkItems>
-                           <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='/'>How it works</FooterLink>
-                               <FooterLink to='/'>Testmonials</FooterLink>
-                               <FooterLink to='/'>Carrers</FooterLink>
-                               <FooterLink to='/'>Investors</FooterLink>
-                               <FooterLink to='/'>Terms of Service</FooterLink>                           
-                       </FooterLinkItems>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/'>How it works</FooterLink>
+                            <FooterLink to='/'>Testmonials</FooterLink>
+                            <FooterLink to='/'>Carrers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
 
-                       <FooterLinkItems>
-                           <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='/'>How it works</FooterLink>
-                               <FooterLink to='/'>Testmonials</FooterLink>
-                               <FooterLink to='/'>Carrers</FooterLink>
-                               <FooterLink to='/'>Investors</FooterLink>
-                               <FooterLink to='/'>Terms of Service</FooterLink>                           
-                       </FooterLinkItems>
-                    </FooterLinksWrapper>  
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/'>How it works</FooterLink>
+                            <FooterLink to='/'>Testmonials</FooterLink>
+                            <FooterLink to='/'>Carrers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
 
                     <FooterLinksWrapper>
-                       <FooterLinkItems>
-                           <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='/'>How it works</FooterLink>
-                               <FooterLink to='/'>Testmonials</FooterLink>
-                               <FooterLink to='/'>Carrers</FooterLink>
-                               <FooterLink to='/'>Investors</FooterLink>
-                               <FooterLink to='/'>Terms of Service</FooterLink>                           
-                       </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/'>How it works</FooterLink>
+                            <FooterLink to='/'>Testmonials</FooterLink>
+                            <FooterLink to='/'>Carrers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
 
-                       <FooterLinkItems>
-                           <FooterLinkTitle>About Us</FooterLinkTitle>
-                               <FooterLink to='/'>How it works</FooterLink>
-                               <FooterLink to='/'>Testmonials</FooterLink>
-                               <FooterLink to='/'>Carrers</FooterLink>
-                               <FooterLink to='/'>Investors</FooterLink>
-                               <FooterLink to='/'>Terms of Service</FooterLink>                           
-                       </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/'>How it works</FooterLink>
+                            <FooterLink to='/'>Testmonials</FooterLink>
+                            <FooterLink to='/'>Carrers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
 
-                    </FooterLinksWrapper>                     
+                    </FooterLinksWrapper>
                 </FooterLinksContainer>
 
                 <SocialMedia>
                     <SocialMediaWrap>
-                    <SocialLogo to='/'>
-                    GymTech
-                    </SocialLogo>
-                    <WebsiteRights> GymTech  ©  {new Date().getFullYear() } 
-                     All rigths reserved</WebsiteRights>
+                        <SocialLogo to='/'>
+                            GymTech
+                        </SocialLogo>
+                        <WebsiteRights> GymTech  ©  {new Date().getFullYear()}
+                            All rigths reserved</WebsiteRights>
 
-                    <SocialIcons>
-                        <SocialIconLink href="//www.facebook.com/resilia.br/" target="_blank"
-                        aria-label="Facebook">
-                        <FaFacebook />
-                        </SocialIconLink>
+                        <SocialIcons>
+                            <SocialIconLink href="//www.facebook.com/resilia.br/" target="_blank"
+                                aria-label="Facebook">
+                                <FaFacebook />
+                            </SocialIconLink>
 
-                        <SocialIconLink href="//https://www.instagram.com/resilia.br/" target="_blank"
-                        aria-label="Instagram">
-                        <FaInstagram />
-                        </SocialIconLink>
+                            <SocialIconLink href="//https://www.instagram.com/resilia.br/" target="_blank"
+                                aria-label="Instagram">
+                                <FaInstagram />
+                            </SocialIconLink>
 
-                        <SocialIconLink href="//www.youtube.com/channel/UCBIRxu3dixMNMRVjz-YQAcA" target="_blank"
-                        aria-label="Youtube">
-                        <FaYoutube />
-                        </SocialIconLink>
+                            <SocialIconLink href="//www.youtube.com/channel/UCBIRxu3dixMNMRVjz-YQAcA" target="_blank"
+                                aria-label="Youtube">
+                                <FaYoutube />
+                            </SocialIconLink>
 
-                        <SocialIconLink href="//twitter.com/resilia_br" target="_blank"
-                        aria-label="Twitter">
-                        <FaTwitter />
-                        </SocialIconLink>
+                            <SocialIconLink href="//twitter.com/resilia_br" target="_blank"
+                                aria-label="Twitter">
+                                <FaTwitter />
+                            </SocialIconLink>
 
-                        <SocialIconLink href="//www.linkedin.com/company/resilia-educa%C3%A7%C3%A3o/" target="_blank"
-                        aria-label="Linkedin">
-                        <FaLinkedin />
-                        </SocialIconLink>
-                        
-                    </SocialIcons>
+                            <SocialIconLink href="//www.linkedin.com/company/resilia-educa%C3%A7%C3%A3o/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialIconLink>
+
+                        </SocialIcons>
 
                     </SocialMediaWrap>
-                    </SocialMedia>
+                </SocialMedia>
 
             </FooterWrap>
         </FooterContainer>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,52 +1,76 @@
-import React from 'react';
-import {FaBars} from 'react-icons/fa'
-import {Nav, 
-    NavbarContainer, 
-    NavLogo, 
-    MobileIcon, 
-    NavMenu, 
-    NavItem, 
+import React, { useState, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
+import {
+    Nav,
+    NavbarContainer,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
     NavLinks,
     NavBtn,
-    NavBtnLink} from './NavbarElements';
+    NavBtnLink,
+} from './NavbarElements';
 
 const Navbar = ({ toggle }) => {
+    const [scrollNav, setScrollNav] = useState(false);
+
+    const changeNav = () => {
+        if (window.scrollY >= 80) {
+            setScrollNav(true);
+        } else {
+            setScrollNav(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav);
+    }, []);
+
+    const toggleHome = () => {
+        scroll.scrollToTop();
+    };
+
     return (
         <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to="/">GymTech</NavLogo>
-                <MobileIcon onClick={toggle}>
-                    <FaBars />    
-                </MobileIcon>
-                <NavMenu>
-                    <NavItem>
-                        <NavLinks to='about'>About</NavLinks>
-                    </NavItem>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav scrollNav={scrollNav}>
+                    <NavbarContainer>
+                        <NavLogo to='/' onClick={toggleHome}>GymTech</NavLogo>
+                        <MobileIcon onClick={toggle}>
+                            <FaBars />
+                        </MobileIcon>
+                        <NavMenu>
+                            <NavItem>
+                                <NavLinks to='about'>About</NavLinks>
+                            </NavItem>
 
-                    <NavItem>
-                        <NavLinks to='locations'>Locations</NavLinks>
-                    </NavItem>
+                            <NavItem>
+                                <NavLinks to='locations'>Locations</NavLinks>
+                            </NavItem>
 
-                    <NavItem>
-                        <NavLinks to='memberships'>Memberships</NavLinks>
-                    </NavItem>
+                            <NavItem>
+                                <NavLinks to='memberships'>Memberships</NavLinks>
+                            </NavItem>
 
-                    <NavItem>
-                        <NavLinks to='howitworks'>How it Works</NavLinks>
-                    </NavItem>
+                            <NavItem>
+                                <NavLinks to='howitworks'>How it Works</NavLinks>
+                            </NavItem>
 
-                    <NavItem>
-                        <NavLinks to='/contact'>Contact</NavLinks>
-                    </NavItem>
-                </NavMenu>
-                <NavBtn>
-                    <NavBtnLink to='/login'>Log In</NavBtnLink>
-                </NavBtn>
-            </NavbarContainer>
-        </Nav>
+                            <NavItem>
+                                <NavLinks to='/contact'>Contact</NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                        <NavBtn>
+                            <NavBtnLink to='/login'>Log In</NavBtnLink>
+                        </NavBtn>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
         </>
-    )
-}
+    );
+};
 
 export default Navbar;
