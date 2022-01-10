59d4ddc16fc1f885455b4135c70b03380c3f1f0a
Murilo <murilomininel1@hotmail.com>
2022-01-10 14:52:12 -0300

    fazendo o form

---
+++ src/components/Login/SignUp/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SignUp() {
+    return (
+        <form>
+            <label for='nome'>Name</label>
+            <input type='text' id='nome' />
+        </form>
+    )
+}
+
+export default SignUp
