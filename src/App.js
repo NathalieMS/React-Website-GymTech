92331413e1e11cc23923da8f09dd25638f775aa7
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-10 18:00:14 -0300

    tentativa de salvar a main

--- src/App.js
+++ src/App.js
@@ -2,38 +2,6 @@ import './App.css';
 import {pegaLista} from "./service/lista"
 
 
-function App() {
-  const [alert, serAlert]= useState(false);
-  const [itemInput, setItemInput] = useState('');
-  const [list, fazLista] = useState([]);
-  let montado =true;
-  
-  useEffect(() =>{
-    pegaLista()
-    .then(aluno =>{
-      if(montado){
-        fazLista(aluno)
-      }
-    })
-    return () => montado = false;
-  },[])
-
-  return(
-    <div className= "wrapper">
-    <h1>Lista de alunos</h1>
-    <ul>
-      {list.map(aluno => <li key={aluno.aluno}>{aluno.aluno}</li>)}
-    </ul>
-    <form>
-       <label>
-         <p>Novo Item</p>
-         <input type="text" />
-       </label>
-       <button type="submit">Submit</button>
-     </form>
-    </div>
-  );
-}
 
 //comentario da API teste
 
