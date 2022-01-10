ff6542410e93a69c3cb3010f4cf1c44ef5ed370d
Murilo <murilomininel1@hotmail.com>
2022-01-10 19:41:59 -0300

    legebilidade

--- src/components/Login/APi/ReqApi.jsx
+++ src/components/Login/APi/ReqApi.jsx
@@ -16,7 +16,19 @@ function ReqApi() {
   return (<div>
     This is a<button onClick={Alunos}>GET</button>
     <div>
-      {alunos.map((aluno) => { return <ul><li><p>{aluno.nome}</p></li> <p>CPF: {aluno.cpf}</p><p>Adress: {aluno.cidade}</p><p>State: {aluno.estado}</p><p>Telephone: {aluno.telefone}</p><p>E-mail: {aluno.email}</p><p> Plan: {aluno.plano}</p></ul> })}
+      {alunos.map((aluno) => {
+        return <ul>
+          <li>
+            <p>{aluno.nome}</p>
+          </li>
+          <p>CPF: {aluno.cpf}</p>
+          <p>Adress: {aluno.cidade}</p>
+          <p>State: {aluno.estado}</p>
+          <p>Telephone: {aluno.telefone}</p>
+          <p>E-mail: {aluno.email}</p>
+          <p> Plan: {aluno.plano}</p>
+        </ul>
+      })}
     </div>
   </div>);
 }
