5513b16ea4c67b20ed4e96fe21b30cfea7424568
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-11 18:51:44 -0300

    Página de contato e mudança cor navbar

--- src/components/Contact/ContactElements.js
+++ src/components/Contact/ContactElements.js
@@ -10,7 +10,9 @@ right: 0;
 top: 0;
 z-index: 0;
 overflow: hidden;
-background: linear-gradient(135deg, hsla(180, 71%, 1%, 1) 5%, hsla(28, 100%, 50%, 1) 74%, hsla(34, 100%, 50%, 1) 91%);
+background: #f7f8fa;
+/* background: linear-gradient(135deg, hsla(180, 71%, 1%, 1) 5%, hsla(28, 100%, 50%, 1) 74%, hsla(34, 100%, 50%, 1) 91%); */
+/* background: #010606; */
 `;
 
 export const ContactWrap = styled.div`
@@ -26,9 +28,9 @@ justify-content: center;
 
 export const Icon = styled(Link)`
 margin-left: 32px;
-margin-top: -600px;
+margin-top: -150px;
 text-decoration: none;
-color:#fff;
+color:#ff7900;
 font-weight: 700;
 font-size: 32px;
 
@@ -37,4 +39,83 @@ font-size: 32px;
     margin-top: 8px;
 }
 `;
+
+export const ContactContainer = styled.div` 
+    max-width: 1000px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    align-items: center;
+    grid-gap: 48px;
+    padding: 0 50px;
+
+    @media screen and (max-width: 1000px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media screen and (max-width: 768px) {
+        grid-template-columns: 1fr;
+        padding: 0 20px;
+    }
+
+`;
+
+export const ContactCard = styled.div` 
+background: rgb(255,72,0);
+background: linear-gradient(325deg, rgba(255,72,0,0.5357493339132529) 0%, rgba(255,121,0,0.54) 35%, rgba(255,170,0,0.54) 100%);
+
+margin-top: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 10px;
+    height: 380px;
+    padding: 30px;
+    box-shadow: 0 1px 3px -2px rgba(0,0,0,0.2);
+    transition: all 0.2s ease-in-out;
+    /* filter: grayscale(100%); */
+
+    &:hover {
+        /* filter: none; */
+        transform: scale(1.1);
+        transition: all 0.2s ease-in-out;        
+    }
+`;
+
+export const ContactPhoto = styled.img` 
+    height: 160px;
+    width: 160px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+`;
+
+export const ContactH2 = styled.h2` 
+    font-size: 1.5rem;
+    text-align: center;
+    color: #fff;
+    margin-top: 18px;
+    margin-bottom: 10px;
+`;
+
+export const SocialContactContainer = styled.div` 
+
+    padding: 10px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-start ;
+`;
+
+
+export const SocialMediaLink = styled.a` 
+    color: #fff;
+    font-size: 32px;
+
+    &:hover{
+        color: #010606;
+    }
+`;
+
+
 
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import FotoLarissa from '../../images/larissa.jpg'
+import FotoNathalie from '../../images/nathalie.jpg'
+import FotoMurilo from '../../images/murilo.jpg'
+import FotoYasmin from '../../images/yasmin.jpg'
+
+
+import {
+    FaLinkedin,
+    FaGithub
+}
+    from 'react-icons/fa';
 import {
     Container,
     ContactWrap,
-    Icon
+    Icon,
+    ContactContainer,
+    ContactCard,
+    ContactPhoto,
+    ContactH2,
+    SocialContactContainer,
+    SocialMediaLink
    
 } from '././ContactElements'
 
@@ -13,6 +30,81 @@ const Contact = () => {
             <Container>
                 <ContactWrap>
                     <Icon to='/'>GymTech</Icon>
+
+                    <ContactContainer>
+                        
+                    <ContactCard>
+                    <ContactPhoto src={FotoLarissa} />
+                    <ContactH2>Larissa Silva</ContactH2>
+
+                    <SocialContactContainer>
+                    <SocialMediaLink href="//https://www.linkedin.com/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialMediaLink>
+
+                            <SocialMediaLink href="//https://www.github.com/" target="_blank"
+                                aria-label="GitHub">
+                                <FaGithub />
+                            </SocialMediaLink>
+                    </SocialContactContainer>
+                </ContactCard>
+
+                <ContactCard>
+                    <ContactPhoto src={FotoMurilo} />
+                    <ContactH2>Murilo Mininel</ContactH2>
+
+                    <SocialContactContainer>
+                    <SocialMediaLink href="//https://www.linkedin.com/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialMediaLink>
+
+                            <SocialMediaLink href="//https://www.github.com/" target="_blank"
+                                aria-label="GitHub">
+                                <FaGithub />
+                            </SocialMediaLink>
+                    </SocialContactContainer>
+                </ContactCard>
+
+                <ContactCard>
+                    <ContactPhoto src={FotoNathalie} />
+                    <ContactH2>Nathalie Moreira</ContactH2>
+
+                    <SocialContactContainer>
+                    <SocialMediaLink href="//https://www.linkedin.com/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialMediaLink>
+
+                            <SocialMediaLink href="//https://www.github.com/" target="_blank"
+                                aria-label="GitHub">
+                                <FaGithub />
+                            </SocialMediaLink>
+                    </SocialContactContainer>
+                </ContactCard>
+
+                <ContactCard>
+                    <ContactPhoto src={FotoYasmin} />
+                    <ContactH2>Yasmin Reis</ContactH2>
+
+                    <SocialContactContainer>
+                    <SocialMediaLink href="//https://www.linkedin.com/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin />
+                            </SocialMediaLink>
+
+                            <SocialMediaLink href="//https://www.github.com/" target="_blank"
+                                aria-label="GitHub">
+                                <FaGithub />
+                            </SocialMediaLink>
+                    </SocialContactContainer>
+                </ContactCard>
+
+
+
+                    </ContactContainer>
+
                 </ContactWrap>
             </Container>
         </>
