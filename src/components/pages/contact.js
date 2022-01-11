66e21c1a02bfda2b5aeb3c0df1a764cac21bdc1b
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-11 00:25:22 -0300

    Criação de páginas para page de contato

---
+++ src/components/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LogIn from '../LogIn';
+import ScrollToTop from '../ScrollToTop';
+
+
+const LoginPage = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <LogIn />
+        </>
+    )
+}
+
+export default LoginPage;
