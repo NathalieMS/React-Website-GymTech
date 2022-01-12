0313a0b8cf51f07d96fdbcce21e715c0dd4868dd
yasminreisk <yasmin.reiss16@gmail.com>
2022-01-12 00:52:48 -0300

    Atualizacoes no put

--- src/components/Login/APi/ReqApi.jsx
+++ src/components/Login/APi/ReqApi.jsx
@@ -1,9 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Axios from 'axios'
 import styled from 'styled-components'
 
 function ReqApi() {
   const [alunos, setAlunos] = useState([])
+  // estados de disabled
+  const [disabledNome, setDisabledNome] = useState(true)
+  const [disabledCPF, setDisabledCPF] = useState(true)
+  const [disabledEndereco, setDisabledEndereco] = useState(true)
+  const [disabledEstado, setDisabledEstado] = useState(true)
+  const [disabledTelefone, setDisabledTelefone] = useState(true)
+  const [disabledEmail, setDisabledEmail] = useState(true)
+  const [disabledPlano, setDisabledPlano] = useState(true)
+
+  // estados de campos
+  const [name, setName] = useState('')
+  const [cpf, setCpf] = useState('')
+  const [address, setAddress] = useState('')
+  const [state, setState] = useState('')
+  const [phone, setPhone] = useState('')
+  const [plan, setEmail] = useState('')
+  const [email, setPlan] = useState('')
+  
+  
+
   const Alunos = () => {
     Axios.get("https://api-academia-alunos.herokuapp.com/alunos").then((response) => {
       setAlunos(response.data.alunos);
@@ -19,28 +39,53 @@ function ReqApi() {
   }
 
   const PutApi = (id) =>{
-    Axios.put(`https://api-academia-alunos.herokuapp.com/alunos/${id}`).then (() =>{
+    Axios.put(`https://api-academia-alunos.herokuapp.com/alunos/${id}`, {
+      nome: name,
+      cpf: cpf,
+      endereco: address,
+      estado: state,
+      telefone: phone,
+      email: email,
+      idade: null,
+      plano: plan
+  }).then (() =>{
       Alunos ()
     })
   }
 
-  if (alunos) {
-    console.log(alunos)
+  function handleChange({target: { name, value }}) {
+    if(name === 'name') setName(value)
+    if(name === 'cpf') setCpf(value)
+    if(name === 'adress') setAddress(value)
+    if(name === 'state') setState(value)
+    if(name === 'phone') setPhone(value)
+    if(name === 'email') setEmail(value)
+    if(name === 'plan') setPlan(value)
   }
+
   return (<Container>
     This is a<button onClick={Alunos}>GET</button>
     <div>
       {alunos.map((aluno) => {
         return <ul>
           <li>
-            <p>{aluno.nome}</p>
+            <input type="text" name="name" placeholder={aluno.nome} disabled={disabledNome} />
           </li>
           <p>CPF: {aluno.cpf}</p>
-          <p>Address: {aluno.endereco}</p>
-          <p>State: {aluno.estado}</p>
-          <p>Phone: {aluno.telefone}</p>
-          <p>Email: {aluno.email}</p>
-          <p>Plan: {aluno.plano}</p>
+          <input
+            type="text"
+            name="cpf"
+            placeholder={aluno.cpf}
+            disabled={disabledCPF}
+            onChange={handleChange}
+            />
+          {/* repetir botao de edicao para todos */}
+          <button onClick={() => setDisabledCPF(!disabledCPF)}>Editar</button>
+          <input type="text" name="address" value={aluno.endereco} disabled={disabledEndereco} />
+          <input type="text" name="state" value={aluno.estado} disabled={disabledEstado} />
+          <input type="text" name="phone" value={aluno.telefone} disabled={disabledTelefone} />
+          <input type="text" name="email" value={aluno.email} disabled={disabledEmail} />
+          <input type="text" name="plan" value={aluno.plano} disabled={disabledPlano} />
 
           This is a<button onClick={()=>DelApi(aluno.id)}>Delete</button> 
           This is a<button onClick={()=>PutApi(aluno.id)}>Edit</button> 
