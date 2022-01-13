d1941ef9a7b090f6c49d8c35404f20fa37f3eaeb
NathalieMS <nathalie.moreira@hotmail.com>
2022-01-13 15:55:03 -0300

    Finalizacao pagina contato

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Contact from '../Contact';
 import ScrollToTop from '../ScrollToTop';
+import Footer from '../Footer';
+import Sidebar from '../Sidebar';
+import Navbar from '../Navbar';
 
 
 const ContactPage = () => {
+
+    const[isOpen, setIsOpen] = useState(false);
+
+const toggle = () =>{
+    setIsOpen(!isOpen);
+};
+
     return (
         <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
             <ScrollToTop />
             <Contact />
+            <Footer />
         </>
     )
 }
